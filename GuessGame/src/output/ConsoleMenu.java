17894a795802022d88dcf9ed989acf21e011ae7e
mbrehhov <A00325954@student.tus.ie>
2024-10-03 23:21:39 +0100

    adding small sample menu, and some input validation

--- GuessGame/src/output/ConsoleMenu.java
+++ GuessGame/src/output/ConsoleMenu.java
@@ -1,12 +1,47 @@
 package output;
 
+import java.util.Scanner;
+
 public class ConsoleMenu {
        
-    
-
-    public void printmenu()
+    public void execute()
     {
-        System.out.println("MENU");    
+        Scanner input  =null;
+        int selection;
+        boolean exit = false;
+
+        while(exit!=true)
+        {
+            printmenu();
+            
+            try  {
+                 input = new Scanner(System.in);
+                selection = input.nextInt();
+                if(selection == 3) exit = true;
+
+                System.out.println(selection);   
+            }
+            catch (Exception e)
+            {
+                System.out.println("wrong option try again");
+            }
+            
+            
+        }
+        if(input != null) input.close();
+
+        
+      
+    }
+
+    private void printmenu()
+    {
+            
+            System.out.println("#########################");
+            System.out.println("[1] new game");
+            System.out.println("[2] see score");
+            System.out.println("[3] exit");
+            System.out.println("#########################");        
     }
 
 }
--- GuessGame/src/test/App.java
+++ GuessGame/src/test/App.java
@@ -5,7 +5,7 @@ import output.ConsoleMenu;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        new ConsoleMenu().printmenu();
+        new ConsoleMenu().execute();
         
 
     }
