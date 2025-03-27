7935c86e239c5889412fd4cbfc18e1773e9bc7b7
mbrehhov <A00325954@student.tus.ie>
2025-03-27 19:23:52 +0000

    cleaning

--- GuessGame/src/output/GamePanel.java
+++ GuessGame/src/output/GamePanel.java
@@ -7,7 +7,6 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -24,139 +23,113 @@ import javax.swing.border.TitledBorder;
 
 public class GamePanel {
 
- JPanel mainp;
- JTextArea jTextAreaQuestions;
- Set<JRadioButton> options;
- List<JLabel> hearts  = new ArrayList<>();
- JLabel timeLabel;
+    JPanel mainp;
+    JTextArea jTextAreaQuestions;
+    Set<JRadioButton> options;
+    List<JLabel> hearts = new ArrayList<>();
+    JLabel timeLabel;
 
     public List<JLabel> getHearts() {
-    return hearts;
-}
+        return hearts;
+    }
 
-    public GamePanel(GameFrame mainFrame)
-    {
-             //create a main paenl (with grid)
+    public GamePanel(GameFrame mainFrame) {
+        // create a main paenl (with grid)
         mainp = new JPanel();
         mainp.setBorder(new TitledBorder("Guessing Game"));
-        mainp.setLayout(new GridLayout(3,0));
+        mainp.setLayout(new GridLayout(3, 0));
 
-        
         mainp.setBackground(Color.red);
 
+        jTextAreaQuestions = new JTextArea(10, 40); // for question panel
 
-         jTextAreaQuestions = new JTextArea(10, 40);  // for question panel
-
-        //JButton button1, button2;
+        // JButton button1, button2;
         JButton button2;
-    
 
         options = new HashSet<>();
-        JRadioButton birdButton =  new JRadioButton("");
-        JRadioButton catButton = new JRadioButton("");
-        JRadioButton dogButton = new JRadioButton("");
-        JRadioButton rabbitButton = new JRadioButton("");
+        JRadioButton option1 = new JRadioButton("");
+        JRadioButton option2 = new JRadioButton("");
+        JRadioButton option3 = new JRadioButton("");
+        JRadioButton option4 = new JRadioButton("");
 
-
-        options.add(birdButton);
-        options.add(catButton);
-        options.add(dogButton);
-        options.add(rabbitButton);
-
-
+        options.add(option1);
+        options.add(option2);
+        options.add(option3);
+        options.add(option4);
 
         JPanel panelQuestion = new JPanel();
         panelQuestion.setBorder(new TitledBorder("question"));
-        panelQuestion.setLayout(new GridLayout(1,1));
+        panelQuestion.setLayout(new GridLayout(1, 1));
 
         panelQuestion.add(jTextAreaQuestions);
 
         mainp.add(panelQuestion);
 
+        JPanel panelOptions = new JPanel();
 
-                JPanel panelOptions = new JPanel();
-       
-        birdButton.setMnemonic(KeyEvent.VK_B);
-        //birdButton.setActionCommand(option1.getText());
-        birdButton.setSelected(true);
-
-        catButton.setMnemonic(KeyEvent.VK_C);
-        //catButton.setActionCommand(option2.getText());
-        dogButton.setMnemonic(KeyEvent.VK_D);
-        //dogButton.setActionCommand(option3.getText());
-
-        rabbitButton.setMnemonic(KeyEvent.VK_R);
-        //rabbitButton.setActionCommand(option4.getText());
- 
-     //Group the radio buttons.
-         ButtonGroup group = new ButtonGroup();
-         group.add(birdButton);
-         group.add(catButton);
-         group.add(dogButton);
-         group.add(rabbitButton);
-       
-
+        // Group the radio buttons.
+        ButtonGroup group = new ButtonGroup();
+        group.add(option1);
+        group.add(option2);
+        group.add(option3);
+        group.add(option4);
 
         JPanel radioButnPanel = new JPanel();
 
-        radioButnPanel.add(birdButton);
-        radioButnPanel.add(catButton);
-        radioButnPanel.add(dogButton);
-        radioButnPanel.add(rabbitButton);
-       
+        radioButnPanel.add(option1);
+        radioButnPanel.add(option2);
+        radioButnPanel.add(option3);
+        radioButnPanel.add(option4);
+
         radioButnPanel.setLayout(new BoxLayout(radioButnPanel, BoxLayout.Y_AXIS));
-        radioButnPanel.setAlignmentX( Component.LEFT_ALIGNMENT );//0.0
-        
+        radioButnPanel.setAlignmentX(Component.LEFT_ALIGNMENT);// 0.0
+
         JPanel panelOptionsR = new JPanel();
         panelOptionsR.setLayout(new BorderLayout());
-        panelOptionsR.add(radioButnPanel,BorderLayout.CENTER);
-        //panelOptionsR.add(new JButton("Smit"), BorderLayout.SOUTH);
+        panelOptionsR.add(radioButnPanel, BorderLayout.CENTER);
+        // panelOptionsR.add(new JButton("Smit"), BorderLayout.SOUTH);
 
         panelOptions.add(panelOptionsR);
         mainp.add(panelOptions);
 
-           //Register a listener for the radio buttons.
-        birdButton.addActionListener(mainFrame);
-        catButton.addActionListener(mainFrame);
-        dogButton.addActionListener(mainFrame);
-        rabbitButton.addActionListener(mainFrame);
+        // Register a listener for the radio buttons.
+        option1.addActionListener(mainFrame);
+        option2.addActionListener(mainFrame);
+        option3.addActionListener(mainFrame);
+        option4.addActionListener(mainFrame);
 
-        JPanel panel3  = new JPanel();
+        JPanel panel3 = new JPanel();
 
-
-        timeLabel = new JLabel("",JLabel.RIGHT);
+        timeLabel = new JLabel("", JLabel.RIGHT);
         timeLabel.setVerticalAlignment(JLabel.TOP);
         timeLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
         timeLabel.setPreferredSize(new Dimension(250, 100));
         timeLabel.setForeground(new Color(70, 90, 40));
         timeLabel.setBackground(new Color(100, 20, 70));
 
-
-        //button1 = new JButton("new");
+        // button1 = new JButton("new");
         button2 = new JButton("Submit");
-        //button1.addActionListener(mainFrame);
+        // button1.addActionListener(mainFrame);
         button2.addActionListener(mainFrame);
-        
-        //panel3.add(button1);
-        
+
+        // panel3.add(button1);
+
         jTextAreaQuestions.setEditable(false);
         panel3.add(timeLabel);
 
         panel3.add(button2);
 
-        
         // setbackground of panel
         panel3.setBackground(Color.BLACK);
         String imgPath = System.getProperty("user.dir") + "/img/Hearth.png";
-        
-            for (int i = 0; i < 4; i++) {
-                JLabel heart = new JLabel(new ImageIcon(imgPath));
-                heart.setVisible(false);
-                hearts.add(heart);
-                panel3.add(heart);
-            }
 
-        
+        for (int i = 0; i < 4; i++) {
+            JLabel heart = new JLabel(new ImageIcon(imgPath));
+            heart.setVisible(false);
+            hearts.add(heart);
+            panel3.add(heart);
+        }
+
         mainp.add(panel3);
 
     }
@@ -176,9 +149,5 @@ public class GamePanel {
     public JLabel getTimeLabel() {
         return timeLabel;
     }
-   
-
-    
-
 
 }
